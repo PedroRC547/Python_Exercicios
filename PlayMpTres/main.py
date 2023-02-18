a2921893ac49b085b2196fa3f3a4c8b5290c2700
PedroRC547 <candianipedro547@gmail.com>
2023-02-17 23:38:39 -0300

    Player de MPTres

---
+++ PlayMpTres/main.py
@@ -0,0 +1,28 @@
+import os
+from playsound import playsound
+
+playListDeMusicas = {}
+
+contador = 0;
+
+pasta = 'playlist'
+
+musicas = os.listdir(pasta)
+
+print("\n")
+print(("-")*60)
+for musica in musicas:
+
+    contador += 1
+
+    print(contador,"-",musica)
+
+    playListDeMusicas[contador] = musica
+    
+print(("-")*60)
+print("\n")
+escolhaMusica = int(input("Musica: "))
+opcao = playListDeMusicas.get(escolhaMusica)
+print('Musica '+' '+opcao+' '+'selecionda')
+print('O audio está sendo reproduzido...')
+playsound(pasta+'/'+opcao)
