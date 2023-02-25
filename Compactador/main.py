4f5361fed80d1dafcec41ac6607081d133e5e3d3
PedroRC547 <candianipedro547@gmail.com>
2023-02-25 20:31:20 -0300

    Descompactar

--- Compactador/main.py
+++ Compactador/main.py
@@ -1,4 +1,25 @@
 from shutil import make_archive
-localizacaoArquivoCompactar = input('Caminho do Arquivo a ser compactado: ')
-nomeArquivoCompactar = input('Como deve ficar o nome do arquivo compactado: ')
-make_archive(nomeArquivoCompactar, 'zip', localizacaoArquivoCompactar)
+from zipfile import ZipFile
+
+while (True):
+    escolha = input(
+        "1 - Compactar arquivo\n2 - Descompactar arquivo\n3 - Sair\nOpção: ")
+    if (escolha == "1"):
+        localizacaoArquivoCompactar = input(
+            'Caminho do Arquivo a ser compactado: ')
+        nomeArquivoCompactar = input(
+            'Como deve ficar o nome do arquivo compactado: ')
+        make_archive(nomeArquivoCompactar, 'zip', localizacaoArquivoCompactar)
+        continue
+
+    elif (escolha == "2"):
+        localizacaoArquivoCompactar = input(
+            'Caminho do Arquivo a ser descompactado: ')
+        arquivoZip = ZipFile(localizacaoArquivoCompactar, 'r')
+        arquivoZip.extractall()
+        arquivoZip.extractall()
+
+    elif (escolha == "3"):
+        break
+    else:
+        print("Opção invalida, tente novamente")
