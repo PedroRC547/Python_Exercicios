c5417c4954936ccde9ef8c9ea0ea728dd55f9c69
PedroRC547 <candianipedro547@gmail.com>
2023-03-07 23:17:36 -0300

    Adição de menu

--- MediaAritmetica/media.py
+++ MediaAritmetica/media.py
@@ -1,9 +1,28 @@
-qntDeNotas = int(input("Qnt de notas: "))
-somaNotas = 0
+escolha = ''
 
-for i in range(qntDeNotas):
-    nota = float(input(f"{i}° Nota: "))
-    somaNotas += nota
+while (escolha != '0'):
 
-mediaAritmetica = (somaNotas)/qntDeNotas
-print(f"A Média Aritmética é: {mediaAritmetica:.2f}")
+    escolha = input(
+        "---Media---\n1 - Media Aritmetica (Padrão)\n2 - Media Aritmetica (Com peso)\n0 - Sair\n>>> ")
+
+    if (escolha == '1'):
+        qntDeNotas = int(
+            input("\n---Media Aritmetica (Padrão)---\n\nQnt de notas: "))
+        somaNotas = 0
+
+        for i in range(qntDeNotas):
+            nota = float(input(f"{i}° Nota: "))
+            somaNotas += nota
+
+        mediaAritmetica = (somaNotas)/qntDeNotas
+        print(f"A Média Aritmética é: {mediaAritmetica:.2f}")
+    elif (escolha == '2'):
+        qntDeNotas = int(
+            input("\n---Media Aritmetica (Com peso)---\n\nQnt de notas: "))
+
+    elif (escolha == '0'):
+        break
+
+    else:
+        print("Comando Inválido tente novamente!")
+        continue
