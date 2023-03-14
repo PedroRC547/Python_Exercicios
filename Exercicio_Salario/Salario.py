abe3ffdc46cd3cc075c0071c98e8dbf39d9bc109
PedroRC547 <candianipedro547@gmail.com>
2023-03-13 23:25:04 -0300

    Exercicio de python salario

---
+++ Exercicio_Salario/Salario.py
@@ -0,0 +1,8 @@
+numeroDoTrabalhador = int(input("N° do Trabalhador: "))
+horasTrabalhadas = int(input("Horas Trabalhadas: "))
+valorPorHora = float(input("Valor por hora: "))
+
+calc = valorPorHora * horasTrabalhadas
+
+print(f"NUMBER = {numeroDoTrabalhador}")
+print(f"SALARY = U$ {calc}")
