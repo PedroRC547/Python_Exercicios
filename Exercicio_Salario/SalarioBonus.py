bd68225ec13a72eea253a61d1694e3b6b279845c
PedroRC547 <candianipedro547@gmail.com>
2023-03-13 23:40:34 -0300

    Adição do software de Salario Bonus

---
+++ Exercicio_Salario/SalarioBonus.py
@@ -0,0 +1,9 @@
+nomeVendedor = input("Nome do Vendedor: ")
+salarioFixo = float(input("Salario fixo: "))
+totalDeVendas = float(input("Total de Vendas (Em dinheiro): "))
+
+salario_bonus = (totalDeVendas*15)/100
+
+total_salario = salarioFixo+salario_bonus
+
+print(f"Total: {total_salario:.f}")
